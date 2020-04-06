7ae612ca1a27e49ddc590e542d6ca95a502cb8e4
Andrew Triola <andrewtriola@gmail.com>
2020-04-06 15:34:01 -0400

    adding py file

---
+++ covid1.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import config
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import *
+from surprise.model_selection import train_test_split
+from surprise import accuracy
+from sklearn.metrics.pairwise import cosine_similarity
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics import calinski_harabasz_score
+from sklearn.decomposition import PCA
+import random
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score,recall_score,precision_score,f1_score
